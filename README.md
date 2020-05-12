68375c6421cb310b7365f4a5cdf0fd753fa6eac1
Pablo Azurduy <pablo@Pablos-MacBook-Pro.local>
2020-05-11 21:11:12 -0400

    newfeature: wip, building deletion filter algorithm

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Conflict Resolution Module
-An implementation of a conflictFinder module based on the publication of [OLIVIER GUIEU AND JOHN W. CHINNECK 1998](http://www.sce.carleton.ca/faculty/chinneck/docs/GuieuChinneck.pdf)
+An implementation of a `conflictFinder` module based on the publication of Olivier [1]
 The main idea is to provide the python-mip library with a conflict dealing module
 
 ### TODO
@@ -9,3 +9,7 @@ The main idea is to provide the python-mip library with a conflict dealing modul
 
 #### Relaxation module 
 - [ ] Implement a linear punishment relaxation algorithm (based on a hierarchy structure)
+
+
+#### References 
+[1] [OLIVIER GUIEU AND JOHN W. CHINNECK 1998](http://www.sce.carleton.ca/faculty/chinneck/docs/GuieuChinneck.pdf)
--- conflict.py
+++ conflict.py
@@ -1,4 +1,5 @@
 import logging
+from copy import copy, deepcopy
 from typing import List, Tuple, Optional, Union, Dict, Any
 from mip.constants import (
     INF,
@@ -21,58 +22,82 @@ from mip.exceptions import (
 )
 import numpy as np
 
+logger = logging.getLogger(__name__)
 class ConflictFinder:
 
-    def __init__():
-        # TODO
+    def __init__(self):
         pass
 
     def find_iis(self,
                  model: "Model", 
                  method: str = "deletion-filter",) -> ConstrList:
         # check if infeasible 
-        assert model.status == InfeasibleSolution, "model is not infeasible"
+        assert model.status == OptimizationStatus.INFEASIBLE, "model is not infeasible"
         # assert ,is not because time limit 
-        model.emphasis = 1 # (FEASIBILITY)
         if method == "deletion-filter":
             return self.deletion_filter(model)
             
     def deletion_filter(self, 
                         model:"Model")-> ConstrList:
-        # get all constraints
-        constraint_list = model.constrs
-        # 1. create a model with all constraints but one 
-
-
-        # 2. test feasibility, if feasible, return dropped constraint to the set 
-        # 2.1 else removed it permanently 
-
-        pass
-
-
-import sys
-if __name__ == "__main__":
-
-    logger = logging.getLogger('conflict_application')
-    handler = logging.StreamHandler(sys.stdout)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
     
+        # 1. create a model with all constraints but one 
+        aux_model = model.copy()
+        aux_model.objective = 1
+        crt_all = copy(aux_model.constrs)
+
+        aux_model.emphasis = 1 # feasibility
+        aux_model.preprocess = 1 # -1  automatic, 0  off, 1  on.
+        logger.debug('starting deletion_filter algorithm')
+        
+        for inc_crt in crt_all:
+            aux_model.constrs.remove([inc_crt]) # temporally remove inc_crt 
+            aux_model.optimize() 
+            status = aux_model.status
+            # 2. test feasibility, if feasible, return dropped constraint to the set 
+            # 2.1 else removed it permanently 
+            if status == OptimizationStatus.INFEASIBLE:
+                logger.debug('removing permanently {}'.format(inc_crt))
+                continue
+            elif status == OptimizationStatus.FEASIBLE:
+                aux_model.constrs.add([inc_crt])
+        iis = aux_model.constrs
+        return iis  
+
+def build_infeasible_cont_model(num_constraints:int = 10, num_infeasible_sets:int = 20) -> Model:
     # build an infeasible model, based on many redundant constraints 
     mdl = Model(name='infeasible_model_continuous')
     var = mdl.add_var(name='var', var_type=CONTINUOUS, lb=-1000, ub=1000)
-    num_constraints = 10
-    num_infeasible_sets = 20
-    for idx,rand_constraint in enumerate(np.linspace(0,1000,num_constraints)):
-        mdl.add_constr(var>=rand_constraint, name='lower_bound_{}'.format(idx))
-        logger.debug('creating an instance of Auxiliary')
+    
+    
+    for idx,rand_constraint in enumerate(np.linspace(1,1000,num_constraints)):
+        crt = mdl.add_constr(var>=rand_constraint, name='lower_bound_{}'.format(idx))
+        logger.debug('added {} to the model'.format(crt))
     
     num_constraint_inf = int(num_infeasible_sets/num_constraints)
-    for idx,rand_constraint in enumerate(np.linspace(-1000,0,num_constraint_inf)):
-        mdl.add_constr(var<=rand_constraint, name='lower_bound_{}'.format(idx))
-        logger.debug('creating an instance of Auxiliary')
+    for idx,rand_constraint in enumerate(np.linspace(-1000,-1,num_constraint_inf)):
+        crt = mdl.add_constr(var<=rand_constraint, name='lower_bound_{}'.format(idx))
+        logger.debug('added {} to the model'.format(crt))
         
     mdl.emphasis = 1 # feasibility
     mdl.preprocess = 1 # -1  automatic, 0  off, 1  on.
     # mdl.pump_passes TODO configure to feasibility emphasis 
-    mdl.optimize()
+    return mdl
+
+
+import sys
+if __name__ == "__main__":
+    
+    # logger config
+    handler = logging.StreamHandler(sys.stdout)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    
+    # model experiment
+    model = build_infeasible_cont_model()
+    logger.debug(model.status)
+    model.optimize()
+    logger.debug(model.status)
+
+    cf = ConflictFinder()
+    iss = cf.find_iis(model)
+    logger.debug(iis)
