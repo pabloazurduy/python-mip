9ed2b06da46b2019efd2e911d426795fe426c0e6
Pablo Azurduy <pablo@Pablos-MacBook-Pro.local>
2020-07-26 20:11:56 -0400

    newfeature: added some additive filtering tests

--- test/test_conflict.py
+++ test/test_conflict.py
@@ -1,6 +1,6 @@
 import pytest
 import mip
-from conflict import ConflictFinder
+from conflict import ConflictFinder, IISFinderAlgorithm 
 
 
 def test_conflict_finder():
@@ -28,6 +28,30 @@ def test_conflict_finder_iis():
     iis_names = set([crt.name for crt in iis])
     assert set(["lower_bound", "upper_bound"]) == iis_names
 
+def test_conflict_finder_iis_additive_method():
+    mdl = mip.Model(name="infeasible_model_continuous")
+    var_x = mdl.add_var(name="x", var_type=mip.CONTINUOUS, lb=-mip.INF, ub=mip.INF)
+    var_y = mdl.add_var(name="x", var_type=mip.CONTINUOUS, lb=-mip.INF, ub=mip.INF)
+    mdl.add_constr(var_x >= 1, "lower_bound")
+    mdl.add_constr(var_x <= 0, "upper_bound")
+    mdl.add_constr(var_y <= -3, "upper_bound_2")
+
+    cf = ConflictFinder(model=mdl)
+    iis = cf.find_iis(method = IISFinderAlgorithm.ADDITIVE_ALGORITHM)
+    iis_names = set([crt.name for crt in iis])
+    assert set(["lower_bound", "upper_bound"]) == iis_names
+
+def test_conflict_finder_iis_additive_method_two_options():
+    mdl = mip.Model(name="infeasible_model_continuous")
+    var_x = mdl.add_var(name="x", var_type=mip.CONTINUOUS, lb=-mip.INF, ub=mip.INF)
+    mdl.add_constr(var_x >= 1, "lower_bound")
+    mdl.add_constr(var_x <= 0, "upper_bound")
+    mdl.add_constr(var_x <= -3, "upper_bound_2")
+
+    cf = ConflictFinder(model=mdl)
+    iis = cf.find_iis(method = IISFinderAlgorithm.ADDITIVE_ALGORITHM)
+    iis_names = set([crt.name for crt in iis])
+    assert set(["lower_bound", "upper_bound"]) == iis_names or set(["lower_bound", "upper_bound_2"]) == iis_names
 
 def test_conflict_finder_feasible():
     mdl = mip.Model(name="feasible_model")
